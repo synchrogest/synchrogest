7060c2d7c969ed0efeae5d731af49942ad6c8a9a
Leandro Paiva <synchrogest@gmail.com>
2025-11-17 14:20:05 +0000

    Atualização __ini__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,8 +2,10 @@ from app.models.usuario import Usuario
 from app.models.categoria import Categoria
 from app.models.produto import Produto
 from app.models.movimentacao import Movimentacao
-# from app.models.vendas import Vendas
-# from app.models.clientes import Clientes
+from app.models.compra_clientes import CompraCliente
+from app.models.compra_itens import CompraItem
+from app.models.clientes import Cliente
+from app.models.pagamentos import Pagamento
 from app.models.log import Log
 
 # Exportar todos os modelos para facilitar importações
@@ -12,7 +14,9 @@ __all__ = [
     "Categoria",
     "Produto",
     "Movimentacao",
-    "Vendas",
-    "Clientes"
+    "CompraCliente",
+    "CompraItens",
+    "Clientes",
+    "Pagamentos",
     "Log"
 ]
